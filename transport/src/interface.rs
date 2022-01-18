730f9924304d3f7faa52fff4d4b0d6583a57a2e5
Viet Tai <vuviettai@gmail.com>
2022-01-18 16:26:50 +0700

    Add Sync + Send to InterfaceRegistrar

--- transport/src/interface.rs
+++ transport/src/interface.rs
@@ -5,7 +5,7 @@ pub trait InstructionParser {
     fn unpack_instruction(&self,content: &[u8]) -> Result<TransportValue, Box<dyn Error>>;
 }
 
-pub trait InterfaceRegistrar {
+pub trait InterfaceRegistrar: Sync + Send {
     fn register_parser(&mut self, handler: Box<dyn InstructionParser + Send + Sync>);
 }
 
